406b413a199e91a32e36c859287572eb2f72bbbb
Gustavo Zimbrón <pptavozl@gmail.com>
2023-07-20 15:30:01 -0600

    fixes pequeños

--- src/lib/sections/AdminHome/AdminHomeSection.svelte
+++ src/lib/sections/AdminHome/AdminHomeSection.svelte
@@ -1,18 +1,12 @@
 <script>
-
-	import { goto } from "$app/navigation";
-
 </script>
+
 <section class="">
-
-    <h1 class="title">Página de inicio de administrador</h1>
-
+	<h1 class="title">Página de inicio de administrador</h1>
 </section>
 
 <style lang="postcss">
-    .title{
-        @apply w-fit mx-auto block mt-9;
-    }
-    
+	.title {
+		@apply w-fit mx-auto block mt-9;
+	}
 </style>
-
--- src/lib/sections/Cursos/_/CursoItem.svelte
+++ src/lib/sections/Cursos/_/CursoItem.svelte
@@ -1,52 +1,52 @@
 <script>
-	import { createEventDispatcher } from "svelte";
+	import { createEventDispatcher } from 'svelte';
 
+	export let curso;
+	const dispatch = createEventDispatcher();
 
-    export let curso;
-    const dispatch = createEventDispatcher();
+	async function handleClick(id) {
+		const confirmacion = window.confirm('¿Desea eliminar este curso?');
+		if (confirmacion) {
+			await fetch('/api/eliminarCurso', {
+				method: 'POST',
+				body: JSON.stringify(id),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
 
-    async function handleClick(id){
-        const confirmacion = window.confirm('¿Desea eliminar este curso?');
-        if(confirmacion){
-            
-            const response = await fetch("/api/eliminarCurso", {
-                method: 'POST',
-                body: JSON.stringify(id),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-
-            dispatch('eliminarCurso', {
-                id: id
-            });
-
-        }
-    }
-
+			dispatch('eliminarCurso', {
+				id: id
+			});
+		}
+	}
 </script>
 
 <div class="card overflow-hidden">
-    <div class="relative">
-        <button on:click={() => {handleClick(curso.id)}} class="bg-red-700 absolute top-2 right-2 p-1 z-20">Eliminar</button>
-    </div>
-    <header>
+	<div class="relative">
+		<button
+			on:click={() => {
+				handleClick(curso.id);
+			}}
+			class="bg-red-700 absolute top-2 right-2 p-1 z-20">Eliminar</button
+		>
+	</div>
+	<header>
 		<img src={curso.imagenURL} class="bg-black/50 w-full" alt="Post" />
 	</header>
-    <h2 class="p-4">{curso.name}</h2>
-    <hr class="opacity-50" />
+	<h2 class="p-4">{curso.name}</h2>
+	<hr class="opacity-50" />
 	<footer class="p-4 flex justify-start items-center space-x-4">
 		<div class="flex-auto flex justify-between items-center">
 			<p class="italic">{curso.descripcion}</p>
 			<small>
-                {#if curso.activo}
-                    🟢
-                {:else}
-                    🔴
-                {/if}</small>
+				{#if curso.activo}
+					🟢
+				{:else}
+					🔴
+				{/if}</small
+			>
 		</div>
-        <a class="block" href={curso.cursoURL}>Ir al curso</a>
-        
+		<a class="block" href={curso.cursoURL}>Ir al curso</a>
 	</footer>
-    
 </div>
